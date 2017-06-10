c38dc9938ea638601fcc192a6241ae97d9b1adf5
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-09 19:19:16 -0500

    Main storyboard now uses the descriptive viewcontroller

---
+++ RaysHotDogs/HotDogDetailsViewController.cs
@@ -0,0 +1,39 @@
+using Foundation;
+using System;
+using UIKit;
+using RaysHotDogs.Core.Model;
+using RaysHotDogs.Core.Service;
+
+namespace RaysHotDogs
+{
+    public partial class HotDogDetailsViewController : UIViewController
+    {
+        public HotDog SelectedHotDog
+        {
+            set;
+            get;
+        }
+
+        public HotDogDetailsViewController (IntPtr handle) : base (handle)
+        {
+            HotDogDataService hotDogDataService = new HotDogDataService();
+            SelectedHotDog = hotDogDataService.getHotDogById(3);
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            DatabindUI();
+        }
+
+        private void DatabindUI()
+        {
+            UIImage img = UIImage.FromFile("Images/" + SelectedHotDog.ImagePath + ".jpg");
+            HotDogImageView.Image = img;
+            NameLabel.Text = SelectedHotDog.Name;
+            ShortDescriptionLabel.Text = SelectedHotDog.ShortDescription;
+            LongDescriptionText.Text = SelectedHotDog.Description;
+            PriceLabel.Text = "$" + SelectedHotDog.Price.ToString();
+        }
+    }
+}
---
+++ RaysHotDogs/HotDogDetailsViewController.designer.cs
@@ -0,0 +1,100 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace RaysHotDogs
+{
+    [Register ("HotDogDetailsViewController")]
+    partial class HotDogDetailsViewController
+    {
+		[Outlet]
+		[GeneratedCode("iOS Designer", "1.0")]
+		UIKit.UIButton AddToCartButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode("iOS Designer", "1.0")]
+		UIKit.UITextField AmountText { get; set; }
+
+		[Outlet]
+		[GeneratedCode("iOS Designer", "1.0")]
+		UIKit.UIButton CancelButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode("iOS Designer", "1.0")]
+		UIKit.UIImageView HotDogImageView { get; set; }
+
+		[Outlet]
+		[GeneratedCode("iOS Designer", "1.0")]
+		UIKit.UITextView LongDescriptionText { get; set; }
+
+		[Outlet]
+		[GeneratedCode("iOS Designer", "1.0")]
+		UIKit.UILabel NameLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode("iOS Designer", "1.0")]
+		UIKit.UILabel PriceLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode("iOS Designer", "1.0")]
+		UIKit.UILabel ShortDescriptionLabel { get; set; }
+
+		void ReleaseDesignerOutlets()
+		{
+			if (AddToCartButton != null)
+			{
+				AddToCartButton.Dispose();
+				AddToCartButton = null;
+			}
+
+			if (AmountText != null)
+			{
+				AmountText.Dispose();
+				AmountText = null;
+			}
+
+			if (CancelButton != null)
+			{
+				CancelButton.Dispose();
+				CancelButton = null;
+			}
+
+			if (HotDogImageView != null)
+			{
+				HotDogImageView.Dispose();
+				HotDogImageView = null;
+			}
+
+			if (LongDescriptionText != null)
+			{
+				LongDescriptionText.Dispose();
+				LongDescriptionText = null;
+			}
+
+			if (NameLabel != null)
+			{
+				NameLabel.Dispose();
+				NameLabel = null;
+			}
+
+			if (PriceLabel != null)
+			{
+				PriceLabel.Dispose();
+				PriceLabel = null;
+			}
+
+			if (ShortDescriptionLabel != null)
+			{
+				ShortDescriptionLabel.Dispose();
+				ShortDescriptionLabel = null;
+			}
+		}
+    }
+}
