e1df4604b4a303bc2a5c439a80a624c8e2cddc8e
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-11 21:53:44 -0500

    The view controller for the table view.

--- RaysHotDogs/HotDogTableViewController.cs
+++ RaysHotDogs/HotDogTableViewController.cs
@@ -1,13 +1,23 @@
 using Foundation;
 using System;
 using UIKit;
+using RaysHotDogs.Core.Service;
+using RaysHotDogs.DataSource;
 
 namespace RaysHotDogs
 {
     public partial class HotDogTableViewController : UITableViewController
     {
+        HotDogDataService dataService = new HotDogDataService();
         public HotDogTableViewController (IntPtr handle) : base (handle)
         {
+        }
+
+        public override void ViewDidLoad()
+        {
+            var hotDogs = dataService.getAllHotDogs();
+            var datasource = new HotDogDataSource(hotDogs, this);
+            TableView.Source = datasource;
         }
     }
 }
