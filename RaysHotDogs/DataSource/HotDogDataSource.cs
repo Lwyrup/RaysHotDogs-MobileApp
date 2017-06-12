033eed2073a9c68452a2d99c7ac82d6514ae2e91
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-11 21:53:23 -0500

    Created a class to act as the datasource for the new tabel view.

---
+++ RaysHotDogs/DataSource/HotDogDataSource.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UIKit;
+using Foundation;
+using System.Collections.Generic;
+using RaysHotDogs.Core.Model;
+
+namespace RaysHotDogs.DataSource
+{
+    public class HotDogDataSource: UITableViewSource
+    {
+        private List<HotDog> hotDogs;
+        NSString cellIdentifier = new NSString("HotDogCell");
+
+        public HotDogDataSource(List<HotDog> hotDogs, UITableViewController callingController)
+        {
+            this.hotDogs = hotDogs;
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return hotDogs.Count;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            UITableViewCell cell = tableView.DequeueReusableCell(cellIdentifier) as UITableViewCell;
+
+            if (cell == null)
+            {
+                cell = new UITableViewCell(UITableViewCellStyle.Default, cellIdentifier);
+            }
+
+            var hotDog = hotDogs[indexPath.Row];
+            cell.TextLabel.Text = hotDog.Name;
+            cell.ImageView.Image = UIImage.FromFile("Images/hotdog" + hotDog.HotDogId + ".jpg");
+
+            return cell;
+        }
+    }
+}
---
+++ RaysHotDogs/HotDogTableViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace RaysHotDogs
+{
+    public partial class HotDogTableViewController : UITableViewController
+    {
+        public HotDogTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
