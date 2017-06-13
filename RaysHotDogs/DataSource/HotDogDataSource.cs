0abe42a00a08074bfcb99449b54f10e75d8f1169
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-13 14:10:46 -0500

    Call to get one item by id, and finally got the segue firing!!!!!!!!!!!!

--- RaysHotDogs/DataSource/HotDogDataSource.cs
+++ RaysHotDogs/DataSource/HotDogDataSource.cs
@@ -3,17 +3,20 @@ using UIKit;
 using Foundation;
 using System.Collections.Generic;
 using RaysHotDogs.Core.Model;
+using RaysHotDogs.Core.Service;
 
 namespace RaysHotDogs.DataSource
 {
     public class HotDogDataSource: UITableViewSource
     {
         private List<HotDog> hotDogs;
+        private UITableViewController tableController;
         NSString cellIdentifier = new NSString("HotDogCell");
 
         public HotDogDataSource(List<HotDog> hotDogs, UITableViewController callingController)
         {
             this.hotDogs = hotDogs;
+            this.tableController = callingController;
         }
 
         public override nint RowsInSection(UITableView tableview, nint section)
@@ -26,15 +29,27 @@ namespace RaysHotDogs.DataSource
             HotDogListCell cell = tableView.DequeueReusableCell(cellIdentifier) as HotDogListCell;
 
             if (cell == null)
-               cell = new HotDogListCell(cellIdentifier);
+            {
+                cell = new HotDogListCell(cellIdentifier);
+            }
 
             cell.UpdateCell(
                 hotDogs[indexPath.Row].Name, 
                 hotDogs[indexPath.Row].Price.ToString(), 
-                UIImage.FromFile("Images/" + hotDogs[indexPath.Row].HotDogId + ".jpg")
+                UIImage.FromFile("Images/hotdog" + hotDogs[indexPath.Row].HotDogId + ".jpg")
             );
 
             return cell;
+        }
+
+        public HotDog GetItem(int id)
+        {
+            return hotDogs[id];
+        }
+
+        public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
+            tableController.PerformSegue("HotDogDetailSegue",tableController);
         }
     }
 }
