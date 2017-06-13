c65ff3fb9cee29d0e609494a2189f375f7b742cb
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-13 14:11:13 -0500

    Code to pass the selected hotdogs info into the detail view

--- RaysHotDogs/HotDogTableViewController.cs
+++ RaysHotDogs/HotDogTableViewController.cs
@@ -9,15 +9,30 @@ namespace RaysHotDogs
     public partial class HotDogTableViewController : UITableViewController
     {
         HotDogDataService dataService = new HotDogDataService();
-        public HotDogTableViewController (IntPtr handle) : base (handle)
+		public HotDogTableViewController(IntPtr handle) : base (handle)
         {
-        }
+		}
 
         public override void ViewDidLoad()
         {
             var hotDogs = dataService.getAllHotDogs();
             var datasource = new HotDogDataSource(hotDogs, this);
             TableView.Source = datasource;
+        }
+
+        public override void PrepareForSegue(UIStoryboardSegue segue, NSObject sender)
+        {
+            if (segue.Identifier == "HotDogDetailSegue")
+            {
+                var hotDogDetailViewController = segue.DestinationViewController as HotDogDetailsViewController;
+                if (hotDogDetailViewController != null)
+                {
+                    var source = TableView.Source as HotDogDataSource;
+                    var rowPath = TableView.IndexPathForSelectedRow;
+                    var item = source.GetItem(rowPath.Row);
+                    hotDogDetailViewController.SelectedHotDog = item;
+                }
+            }
         }
     }
 }
