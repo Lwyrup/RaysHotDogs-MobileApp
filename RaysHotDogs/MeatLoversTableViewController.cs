15cbb601c70fcf17d3aaef65f3c446212c565e5e
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-13 19:25:12 -0500

    Controller for the meat tab, inherits from base hotdogtableview class.

--- RaysHotDogs/MeatLoversTableViewController.cs
+++ RaysHotDogs/MeatLoversTableViewController.cs
@@ -1,13 +1,24 @@
 using Foundation;
 using System;
 using UIKit;
+using RaysHotDogs.Core.Model;
+using RaysHotDogs.Core.Service;
+using RaysHotDogs.DataSource;
 
 namespace RaysHotDogs
 {
-    public partial class MeatLoversTableViewController : UITableViewController
+    public partial class MeatLoversTableViewController : HotDogTableViewController
     {
+        HotDogDataService dataService = new HotDogDataService();
         public MeatLoversTableViewController (IntPtr handle) : base (handle)
         {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            var meatLovers = dataService.GetMeatLoversHotDogs();
+            TableView.Source = new HotDogDataSource(meatLovers, this);
         }
     }
 }
