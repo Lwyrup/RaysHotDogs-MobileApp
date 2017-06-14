a68f91385de8caff09578d93bb875e13e1385a9d
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-13 19:26:34 -0500

    Added controllers for the tabbed layout

--- RaysHotDogs/FavoriteTableViewController.cs
+++ RaysHotDogs/FavoriteTableViewController.cs
@@ -1,13 +1,24 @@
 using Foundation;
 using System;
 using UIKit;
+using RaysHotDogs.Core.Model;
+using RaysHotDogs.Core.Service;
+using RaysHotDogs.DataSource;
 
 namespace RaysHotDogs
 {
-    public partial class FavoriteTableViewController : UITableViewController
+    public partial class FavoriteTableViewController : HotDogTableViewController
     {
+        HotDogDataService dataService = new HotDogDataService();
         public FavoriteTableViewController (IntPtr handle) : base (handle)
         {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            var favorites = dataService.GetFavoriteHotDogs();
+            TableView.Source = new HotDogDataSource(favorites, this);
         }
     }
 }
--- RaysHotDogs/VeggieLoversTableViewController.cs
+++ RaysHotDogs/VeggieLoversTableViewController.cs
@@ -1,13 +1,24 @@
 using Foundation;
 using System;
 using UIKit;
+using RaysHotDogs.Core.Model;
+using RaysHotDogs.Core.Service;
+using RaysHotDogs.DataSource;
 
 namespace RaysHotDogs
 {
-    public partial class VeggieLoversTableViewController : UITableViewController
+    public partial class VeggieLoversTableViewController : HotDogTableViewController
     {
+        HotDogDataService dataService = new HotDogDataService();
         public VeggieLoversTableViewController (IntPtr handle) : base (handle)
         {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            var veggieLovers = dataService.GetVeggieLoversHotDogs();
+            TableView.Source = new HotDogDataSource(veggieLovers, this);
         }
     }
 }
