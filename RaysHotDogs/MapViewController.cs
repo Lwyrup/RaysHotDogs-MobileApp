b4b68c891aa1ff23d305308cf237203af654fa3b
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-14 18:23:59 -0500

    Created a view for the map

---
+++ RaysHotDogs/MapViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace RaysHotDogs
+{
+    public partial class MapViewController : UIViewController
+    {
+        public MapViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
