f781f8f9484758d1c9c01a1a6ac3e69a834c676e
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-14 19:48:44 -0500

    Added identifiers

---
+++ RaysHotDogs/TweetViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace RaysHotDogs
+{
+    public partial class TweetViewController : UIViewController
+    {
+        public TweetViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
