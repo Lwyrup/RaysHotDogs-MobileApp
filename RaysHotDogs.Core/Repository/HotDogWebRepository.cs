ce8eafd73e1834ed2ed558f7f0e01ce8b6a1a7f1
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-15 21:25:31 -0500

    Created the web repository class.

--- RaysHotDogs.Core/Repository/HotDogWebRepository.cs
+++ RaysHotDogs.Core/Repository/HotDogWebRepository.cs
@@ -1,10 +1,97 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RaysHotDogs.Core.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
 namespace RaysHotDogs.Core.Repository
 {
     public class HotDogWebRepository
     {
+        string uri = "http://gillcleerenpluralsight.blob.core.windows.net/files/hotdogs.json";
+
         public HotDogWebRepository()
         {
+            Task.Run(() => this.LoadDataAsync(uri)).Wait();
         }
+
+		public List<HotDog> GetAllHotDogs()
+		{
+			IEnumerable<HotDog> hotDogs =
+				from hotDogGroup in hotDogGroups
+				from hotDog in hotDogGroup.HotDogs
+
+				select hotDog;
+			return hotDogs.ToList<HotDog>();
+		}
+
+		public HotDog GetHotDogById(int hotDogId)
+		{
+			IEnumerable<HotDog> hotDogs =
+				from hotDogGroup in hotDogGroups
+				from hotDog in hotDogGroup.HotDogs
+				where hotDog.HotDogId == hotDogId
+				select hotDog;
+
+			return hotDogs.FirstOrDefault();
+		}
+
+		public List<HotDogGroup> GetGroupedHotDogs()
+		{
+			return hotDogGroups;
+		}
+
+		public List<HotDog> GetHotDogsForGroup(int hotDogGroupId)
+		{
+			var group = hotDogGroups.Where(h => h.HotDogGroupId == hotDogGroupId).FirstOrDefault();
+
+			if (group != null)
+			{
+				return group.HotDogs;
+			}
+			return null;
+		}
+
+		public List<HotDog> GetFavoriteHotDogs()
+		{
+			IEnumerable<HotDog> hotDogs =
+				from hotDogGroup in hotDogGroups
+				from hotDog in hotDogGroup.HotDogs
+				where hotDog.IsFavorite
+				select hotDog;
+			return hotDogs.ToList<HotDog>();
+		}
+
+        // Variable holding all the data
+        private static List<HotDogGroup> hotDogGroups = new List<HotDogGroup>();
+
+        public async Task LoadDataAsync(string uri)
+        {
+            if (hotDogGroups != null)
+            {
+                string responseJsonString = null;
+                using (var httpClient = new HttpClient())
+                {
+                    try
+                    {
+                        Task<HttpResponseMessage> getResponse = httpClient.GetAsync(uri);
+                        HttpResponseMessage response = await getResponse;
+                        responseJsonString = await response.Content.ReadAsStringAsync();
+                        hotDogGroups = JsonConvert.DeserializeObject<List<HotDogGroup>>(responseJsonString);
+                    }
+                    catch (Exception ex)
+                    {
+                        string message = ex.Message;
+                    }
+                }
+            }
+        }
+
+
     }
 }
