1ea349787b9321e99d163541bca779084ca8326a
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-26 07:10:46 -0500

    Removed starter code

--- RaysHotDogs.android/MainActivity.cs
+++ RaysHotDogs.android/MainActivity.cs
@@ -8,20 +8,10 @@ namespace RaysHotDogs.android
     [Activity(Label = "Ray's Hot Dogs", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
-
-            button.Click += delegate { button.Text = $"{count++} clicks!"; };
         }
     }
 }
