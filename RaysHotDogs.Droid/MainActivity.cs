1ce6596cae83afff7a9db4831c462a84ccfd0017
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-26 17:26:10 -0500

    PLEASE DON'T BREAK

--- RaysHotDogs.Droid/MainActivity.cs
+++ RaysHotDogs.Droid/MainActivity.cs
@@ -2,15 +2,18 @@
 using Android.Widget;
 using Android.OS;
 
-namespace RaysHotDogs.Android
+namespace RaysHotDogs.Droid
 {
     [Activity(Label = "Rays Hot Dogs", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
+        int count = 1;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Main);
+           
         }
     }
 }
