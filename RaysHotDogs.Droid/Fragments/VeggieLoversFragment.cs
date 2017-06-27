e656f4faa439b2976e1757994b41235420605c03
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-27 18:31:22 -0500

    Fragment for veggie lovers

--- RaysHotDogs.Droid/Fragments/VeggieLoversFragment.cs
+++ RaysHotDogs.Droid/Fragments/VeggieLoversFragment.cs
@@ -11,24 +11,35 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using RaysHotDogs.Droid.Adapters;
 
 namespace RaysHotDogs.Droid.Fragments
 {
-    public class VeggieLoversFragment : Fragment
+    public class VeggieLoversFragment : BaseFragment
     {
-        public override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
+		public override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
 
-            // Create your fragment here
-        }
+			// Create your fragment here
+		}
 
-        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-        {
-            // Use this to return your custom view for this Fragment
-            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+		public override void OnActivityCreated(Bundle savedInstanceState)
+		{
+			base.OnActivityCreated(savedInstanceState);
 
-            return base.OnCreateView(inflater, container, savedInstanceState);
-        }
+			FindViews();
+			HandleEvents();
+
+			hotDogs = hotDogDataService.GetVeggieLoversHotDogs();
+			listView.Adapter = new HotDogListAdapter(this.Activity, hotDogs);
+		}
+
+		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			// Use this to return your custom view for this Fragment
+			// return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+			return inflater.Inflate(Resource.Layout.FavoriteHotDogFragment, container, false);
+		}
     }
 }
