761b43ec2e1fbb544625aab4002d53a72d360a59
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-27 17:50:25 -0500

    Created the about view and activity.

--- RaysHotDogs.Droid/AboutActivity.cs
+++ RaysHotDogs.Droid/AboutActivity.cs
@@ -13,14 +13,35 @@ using Android.Widget;
 
 namespace RaysHotDogs.Droid
 {
-    [Activity(Label = "AboutActivity")]
+    [Activity(Label = "About Ray's Hot Dogs")]
     public class AboutActivity : Activity
     {
+        TextView phoneNumberTextView;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.AboutView);
 
-            // Create your application here
+            FindViews();
+            HandleEvents();
+        }
+
+        void FindViews()
+        {
+            phoneNumberTextView = FindViewById<TextView>(Resource.Id.phoneNumberTextView);
+        }
+
+        void HandleEvents()
+        {
+            phoneNumberTextView.Click += PhoneNumberTextView_Click;
+        }
+
+        void PhoneNumberTextView_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(Intent.ActionCall);
+            intent.SetData(Android.Net.Uri.Parse("tel:" + phoneNumberTextView.Text));
+            StartActivity(intent);
         }
     }
 }
