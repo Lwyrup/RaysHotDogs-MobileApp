3f742ecc2b9554ef9234b2a6be9d75712721a1c5
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-27 18:31:03 -0500

    Added fragment for the favorite hotdogs

--- RaysHotDogs.Droid/Fragments/FavoriteHotDogFragment.cs
+++ RaysHotDogs.Droid/Fragments/FavoriteHotDogFragment.cs
@@ -11,10 +11,11 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using RaysHotDogs.Droid.Adapters;
 
 namespace RaysHotDogs.Droid.Fragments
 {
-    public class FavoriteHotDogFragment : Fragment
+    public class FavoriteHotDogFragment : BaseFragment
     {
         public override void OnCreate(Bundle savedInstanceState)
         {
@@ -23,12 +24,22 @@ namespace RaysHotDogs.Droid.Fragments
             // Create your fragment here
         }
 
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            base.OnActivityCreated(savedInstanceState);
+
+            FindViews();
+            HandleEvents();
+
+            hotDogs = hotDogDataService.GetFavoriteHotDogs();
+            listView.Adapter = new HotDogListAdapter(this.Activity, hotDogs);
+        }
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             // Use this to return your custom view for this Fragment
             // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
-
-            return base.OnCreateView(inflater, container, savedInstanceState);
+            return inflater.Inflate(Resource.Layout.FavoriteHotDogFragment, container, false);
         }
     }
 }
