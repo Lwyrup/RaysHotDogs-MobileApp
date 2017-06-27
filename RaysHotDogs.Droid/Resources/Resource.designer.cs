cfc534393373f13f529c37111870e4cfc9e7efd0
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-27 16:47:50 -0500

    Created a main menu view for the app

--- RaysHotDogs.Droid/Resources/Resource.designer.cs
+++ RaysHotDogs.Droid/Resources/Resource.designer.cs
@@ -1411,19 +1411,22 @@ namespace RaysHotDogs.Droid
 			public const int abc_textfield_search_material = 2130837572;
 			
 			// aapt resource value: 0x7f020045
-			public const int hotdog1 = 2130837573;
+			public const int homeimg = 2130837573;
 			
 			// aapt resource value: 0x7f020046
-			public const int hotdog2 = 2130837574;
+			public const int hotdog1 = 2130837574;
 			
 			// aapt resource value: 0x7f020047
-			public const int hotdog3 = 2130837575;
+			public const int hotdog2 = 2130837575;
 			
 			// aapt resource value: 0x7f020048
-			public const int hotdog4 = 2130837576;
+			public const int hotdog3 = 2130837576;
 			
 			// aapt resource value: 0x7f020049
-			public const int notification_template_icon_bg = 2130837577;
+			public const int hotdog4 = 2130837577;
+			
+			// aapt resource value: 0x7f02004a
+			public const int notification_template_icon_bg = 2130837578;
 			
 			static Drawable()
 			{
@@ -1438,8 +1441,11 @@ namespace RaysHotDogs.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f0c005b
-			public const int action0 = 2131492955;
+			// aapt resource value: 0x7f0c005f
+			public const int aboutButton = 2131492959;
+			
+			// aapt resource value: 0x7f0c0060
+			public const int action0 = 2131492960;
 			
 			// aapt resource value: 0x7f0c0041
 			public const int action_bar = 2131492929;
@@ -1465,8 +1471,8 @@ namespace RaysHotDogs.Droid
 			// aapt resource value: 0x7f0c0042
 			public const int action_context_bar = 2131492930;
 			
-			// aapt resource value: 0x7f0c005f
-			public const int action_divider = 2131492959;
+			// aapt resource value: 0x7f0c0064
+			public const int action_divider = 2131492964;
 			
 			// aapt resource value: 0x7f0c0002
 			public const int action_menu_divider = 2131492866;
@@ -1504,14 +1510,17 @@ namespace RaysHotDogs.Droid
 			// aapt resource value: 0x7f0c0057
 			public const int cancelButton = 2131492951;
 			
+			// aapt resource value: 0x7f0c0061
+			public const int cancel_action = 2131492961;
+			
 			// aapt resource value: 0x7f0c005c
-			public const int cancel_action = 2131492956;
+			public const int cartButton = 2131492956;
 			
 			// aapt resource value: 0x7f0c0039
 			public const int checkbox = 2131492921;
 			
-			// aapt resource value: 0x7f0c0062
-			public const int chronometer = 2131492962;
+			// aapt resource value: 0x7f0c0067
+			public const int chronometer = 2131492967;
 			
 			// aapt resource value: 0x7f0c001c
 			public const int collapseActionView = 2131492892;
@@ -1543,8 +1552,8 @@ namespace RaysHotDogs.Droid
 			// aapt resource value: 0x7f0c0019
 			public const int end = 2131492889;
 			
-			// aapt resource value: 0x7f0c0067
-			public const int end_padder = 2131492967;
+			// aapt resource value: 0x7f0c006c
+			public const int end_padder = 2131492972;
 			
 			// aapt resource value: 0x7f0c0025
 			public const int expand_activities_button = 2131492901;
@@ -1576,14 +1585,14 @@ namespace RaysHotDogs.Droid
 			// aapt resource value: 0x7f0c0026
 			public const int image = 2131492902;
 			
-			// aapt resource value: 0x7f0c0066
-			public const int info = 2131492966;
+			// aapt resource value: 0x7f0c006b
+			public const int info = 2131492971;
 			
-			// aapt resource value: 0x7f0c0060
-			public const int line1 = 2131492960;
+			// aapt resource value: 0x7f0c0065
+			public const int line1 = 2131492965;
 			
-			// aapt resource value: 0x7f0c0064
-			public const int line3 = 2131492964;
+			// aapt resource value: 0x7f0c0069
+			public const int line3 = 2131492969;
 			
 			// aapt resource value: 0x7f0c0009
 			public const int listMode = 2131492873;
@@ -1591,11 +1600,17 @@ namespace RaysHotDogs.Droid
 			// aapt resource value: 0x7f0c0028
 			public const int list_item = 2131492904;
 			
+			// aapt resource value: 0x7f0c005b
+			public const int logoImageView = 2131492955;
+			
 			// aapt resource value: 0x7f0c0050
 			public const int mainScrollView = 2131492944;
 			
 			// aapt resource value: 0x7f0c005e
-			public const int media_actions = 2131492958;
+			public const int mapButton = 2131492958;
+			
+			// aapt resource value: 0x7f0c0063
+			public const int media_actions = 2131492963;
 			
 			// aapt resource value: 0x7f0c001a
 			public const int middle = 2131492890;
@@ -1708,8 +1723,8 @@ namespace RaysHotDogs.Droid
 			// aapt resource value: 0x7f0c0017
 			public const int src_over = 2131492887;
 			
-			// aapt resource value: 0x7f0c005d
-			public const int status_bar_latest_event_content = 2131492957;
+			// aapt resource value: 0x7f0c0062
+			public const int status_bar_latest_event_content = 2131492962;
 			
 			// aapt resource value: 0x7f0c004c
 			public const int submit_area = 2131492940;
@@ -1717,17 +1732,20 @@ namespace RaysHotDogs.Droid
 			// aapt resource value: 0x7f0c000b
 			public const int tabMode = 2131492875;
 			
-			// aapt resource value: 0x7f0c0065
-			public const int text = 2131492965;
+			// aapt resource value: 0x7f0c005d
+			public const int takePictureButton = 2131492957;
 			
-			// aapt resource value: 0x7f0c0063
-			public const int text2 = 2131492963;
+			// aapt resource value: 0x7f0c006a
+			public const int text = 2131492970;
+			
+			// aapt resource value: 0x7f0c0068
+			public const int text2 = 2131492968;
 			
 			// aapt resource value: 0x7f0c0034
 			public const int textSpacerNoButtons = 2131492916;
 			
-			// aapt resource value: 0x7f0c0061
-			public const int time = 2131492961;
+			// aapt resource value: 0x7f0c0066
+			public const int time = 2131492966;
 			
 			// aapt resource value: 0x7f0c002a
 			public const int title = 2131492906;
@@ -1879,40 +1897,43 @@ namespace RaysHotDogs.Droid
 			public const int Main = 2130968604;
 			
 			// aapt resource value: 0x7f04001d
-			public const int notification_media_action = 2130968605;
+			public const int MainMenu = 2130968605;
 			
 			// aapt resource value: 0x7f04001e
-			public const int notification_media_cancel_action = 2130968606;
+			public const int notification_media_action = 2130968606;
 			
 			// aapt resource value: 0x7f04001f
-			public const int notification_template_big_media = 2130968607;
+			public const int notification_media_cancel_action = 2130968607;
 			
 			// aapt resource value: 0x7f040020
-			public const int notification_template_big_media_narrow = 2130968608;
+			public const int notification_template_big_media = 2130968608;
 			
 			// aapt resource value: 0x7f040021
-			public const int notification_template_lines = 2130968609;
+			public const int notification_template_big_media_narrow = 2130968609;
 			
 			// aapt resource value: 0x7f040022
-			public const int notification_template_media = 2130968610;
+			public const int notification_template_lines = 2130968610;
 			
 			// aapt resource value: 0x7f040023
-			public const int notification_template_part_chronometer = 2130968611;
+			public const int notification_template_media = 2130968611;
 			
 			// aapt resource value: 0x7f040024
-			public const int notification_template_part_time = 2130968612;
+			public const int notification_template_part_chronometer = 2130968612;
 			
 			// aapt resource value: 0x7f040025
-			public const int select_dialog_item_material = 2130968613;
+			public const int notification_template_part_time = 2130968613;
 			
 			// aapt resource value: 0x7f040026
-			public const int select_dialog_multichoice_material = 2130968614;
+			public const int select_dialog_item_material = 2130968614;
 			
 			// aapt resource value: 0x7f040027
-			public const int select_dialog_singlechoice_material = 2130968615;
+			public const int select_dialog_multichoice_material = 2130968615;
 			
 			// aapt resource value: 0x7f040028
-			public const int support_simple_spinner_dropdown_item = 2130968616;
+			public const int select_dialog_singlechoice_material = 2130968616;
+			
+			// aapt resource value: 0x7f040029
+			public const int support_simple_spinner_dropdown_item = 2130968617;
 			
 			static Layout()
 			{
