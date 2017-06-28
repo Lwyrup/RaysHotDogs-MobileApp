a940416083322770d116632140665cc2a1cd778f
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-27 21:36:00 -0500

    Removed the code that was moved into the basefragment class

--- RaysHotDogs.Droid/HotDogMenuActivity.cs
+++ RaysHotDogs.Droid/HotDogMenuActivity.cs
@@ -18,54 +18,26 @@ using RaysHotDogs.Droid.Fragments;
 
 namespace RaysHotDogs.Droid
 {
-    [Activity(Label = "HotDogMenuActivity")]
+    [Activity(Label = "Menu")]
     public class HotDogMenuActivity : Activity
     {
-        ListView hotDogListView;
-        List<HotDog> allHotDogs;
-        HotDogDataService hotDogDataService;
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             RequestWindowFeature(WindowFeatures.ActionBar);
             ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
-            var colorDrawable = new ColorDrawable(Color.LightGray);
+            var colorDrawable = new ColorDrawable(Color.DarkGray);
             ActionBar.SetStackedBackgroundDrawable(colorDrawable);
 
-            addTab("Favorites", Resource.Drawable.homeimg, new FavoriteHotDogFragment());
-            addTab("Meat Lovers", Resource.Drawable.meatlovericon, new MeatLoversFragment());
-            addTab("Veggie Lovers", Resource.Drawable.homeimg, new VeggieLoversFragment());
+            AddTab("Favorites", Resource.Drawable.homeimg, new FavoriteHotDogFragment());
+            AddTab("Meat Lovers", Resource.Drawable.meatlovericon, new MeatLoversFragment());
+            AddTab("Veggie Lovers", Resource.Drawable.homeimg, new VeggieLoversFragment());
 
             SetContentView(Resource.Layout.HotDogMenuView);
         }
 
-        void HotDogListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
-        {
-            var hotDog = allHotDogs[e.Position];
-            var intent = new Intent(this, typeof(HotDogDetailActivity));
-            intent.PutExtra("selectedHotDogId", hotDog.HotDogId);
-
-            StartActivityForResult(intent, 100);
-        }
-
-        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
-        {
-            base.OnActivityResult(requestCode, resultCode, data);
-
-            if (resultCode == Result.Ok && requestCode == 100)
-            {
-                var selectedHotDog = allHotDogs[data.GetIntExtra("selectedHotDogId", 1)];
-
-                var dialog = new AlertDialog.Builder(this);
-                dialog.SetTitle("Confirmation");
-                dialog.SetMessage(String.Format("You've added {0} order(s) of the {1} to your cart.", data.GetIntExtra("amount", 1), selectedHotDog.Name));
-                dialog.Show();
-            }
-        }
-
-        private void addTab(string tabText, int iconResourceId, Fragment view)
+        void AddTab(string tabText, int iconResourceId, Fragment view)
         {
             var tab = this.ActionBar.NewTab();
             tab.SetText(tabText);
@@ -75,7 +47,9 @@ namespace RaysHotDogs.Droid
             {
                 var fragment = this.FragmentManager.FindFragmentById(Resource.Id.fragmentContainer);
                 if (fragment != null)
+                {
                     e.FragmentTransaction.Remove(fragment);
+                }
                 e.FragmentTransaction.Add(Resource.Id.fragmentContainer, view);
             };
 
